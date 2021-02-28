f8b7115d6b72265a926cd951d823d5a622454515
tate-jenkins <60268138+tate-jenkins@users.noreply.github.com>
2021-02-28 13:20:22 -0800

    functional pop socket

--- Pop_Gen_Gui.py
+++ Pop_Gen_Gui.py
@@ -4,8 +4,10 @@ from tkinter import filedialog
 import csv
 import json
 
-root = Tk()
-root.title("Census Gen")
+
+
+
+
 
 
 def error_window(err_text):
@@ -41,15 +43,19 @@ def close():
     root.destroy()
 
 
-def check_inputs():
+def check_inputs(year, state):
     """
     Checks if year and state inputs are valid and calls
     API, display_data, write_data, clear_inputs if True,
     else calls error_window
     """
     inp_check = True
-    year = e_year.get()
-    state_inp = e_state.get()
+
+    state_inp = state
+
+
+    #year = e_year.get()
+    #state_inp = e_state.get()
     year_list = ['2005', '2006', '2007', '2008', '2009',
                  '2010', '2011', '2012', '2013', '2014',
                  '2015', '2016', '2017', '2018', '2019']
@@ -65,7 +71,7 @@ def check_inputs():
         error_window("Please enter the correct year (2005-2019)")
         return
 
-    state_inp = e_state.get()
+    #state_inp = e_state.get()
     state_inp = state_inp.upper()
     us_state_abbrev = {
         'ALABAMA': 'AL',  # 01
@@ -138,9 +144,9 @@ def check_inputs():
 
     if inp_check:
         api_data = api(str(year), str(location[0]+1))
-        display_data(year, api_data[1][0], api_data[1][1])
+        #display_data(year, api_data[1][0], api_data[1][1])
         write_data(year, api_data[1][0], api_data[1][1])
-        clear_inputs()
+        #clear_inputs()
 
 
 def display_data(year_out, state_out, pop_out):
@@ -162,48 +168,62 @@ def clear_inputs():
     e_year.delete(0, END)
     e_state.delete(0, END)
 
+def gui():
 
-# Create left and right containers
-l_frame = Frame(root, bg='#A9DFBF', width=250, height=450, pady=3,
-                highlightbackground="black", highlightthickness=1)
-r_frame = Frame(root, bg='#E7F0EA', width=600, height=450, pady=3,
-                highlightbackground="black", highlightthickness=1)
-# headers frames for GUI
-header_bar = Frame(r_frame, bg='#DEDEDE', width=200, height=30, pady=3,
-                highlightbackground="#ADB9B0", highlightthickness=1)
-header_bar2 = Frame(r_frame, bg='#DEDEDE', width=200, height=30, pady=3,
-                highlightbackground="#ADB9B0", highlightthickness=1)
-header_bar3 = Frame(r_frame, bg='#DEDEDE', width=200, height=30, pady=3,
-                highlightbackground="#ADB9B0", highlightthickness=1)
-# header labels and placement of year, state, and population
-header_state = Label(header_bar, text="Year", bg="#DEDEDE")
-header_state.place(relx = 0.5, rely = 0.5, anchor='center')
-header_state = Label(header_bar2, text="State", bg="#DEDEDE")
-header_state.place(relx = 0.5, rely = 0.5, anchor='center')
-header_pop = Label(header_bar3, text="Population", bg="#DEDEDE")
-header_pop.place(relx = 0.5, rely = 0.5, anchor='center')
+    root = Tk()
+    root.title("Census Gen")
+    root.mainloop()
+    # Create left and right containers
+    l_frame = Frame(root, bg='#A9DFBF', width=250, height=450, pady=3,
+                    highlightbackground="black", highlightthickness=1)
+    r_frame = Frame(root, bg='#E7F0EA', width=600, height=450, pady=3,
+                    highlightbackground="black", highlightthickness=1)
+    # headers frames for GUI
+    header_bar = Frame(r_frame, bg='#DEDEDE', width=200, height=30, pady=3,
+                    highlightbackground="#ADB9B0", highlightthickness=1)
+    header_bar2 = Frame(r_frame, bg='#DEDEDE', width=200, height=30, pady=3,
+                    highlightbackground="#ADB9B0", highlightthickness=1)
+    header_bar3 = Frame(r_frame, bg='#DEDEDE', width=200, height=30, pady=3,
+                    highlightbackground="#ADB9B0", highlightthickness=1)
+    # header labels and placement of year, state, and population
+    header_state = Label(header_bar, text="Year", bg="#DEDEDE")
+    header_state.place(relx = 0.5, rely = 0.5, anchor='center')
+    header_state = Label(header_bar2, text="State", bg="#DEDEDE")
+    header_state.place(relx = 0.5, rely = 0.5, anchor='center')
+    header_pop = Label(header_bar3, text="Population", bg="#DEDEDE")
+    header_pop.place(relx = 0.5, rely = 0.5, anchor='center')
 
-root.grid_columnconfigure(0, weight=1)
-root.grid_columnconfigure(1, weight=1)
+    root.grid_columnconfigure(0, weight=1)
+    root.grid_columnconfigure(1, weight=1)
 
-# location of frames
-l_frame.grid(row=0, column=0, sticky='nw')
-r_frame.grid(row=0, column=1, columnspan=3, sticky="ne")
-header_bar.grid(row=0, column=0, sticky='nw')
-header_bar2.grid(row=0, column=1, sticky='nw')
-header_bar3.grid(row=0, column=2, sticky='nw')
+    # location of frames
+    l_frame.grid(row=0, column=0, sticky='nw')
+    r_frame.grid(row=0, column=1, columnspan=3, sticky="ne")
+    header_bar.grid(row=0, column=0, sticky='nw')
+    header_bar2.grid(row=0, column=1, sticky='nw')
+    header_bar3.grid(row=0, column=2, sticky='nw')
 
-# label and input field diplay
-year_label = Label(l_frame, text="Input Year", bg='#A9DFBF')
-year_label.place(relx = 0.2, rely = 0.35, anchor='center')
-e_year = Entry(l_frame, width=35, borderwidth=3)
-e_year.place(relx=.5, rely=.4, anchor='center')
-state_label = Label(l_frame, text="Input State", bg='#A9DFBF')
-state_label.place(relx = 0.2, rely = 0.50, anchor='center')
-e_state = Entry(l_frame, width=35, borderwidth=3)
-e_state.place(relx=.5, rely=.55, anchor='center')
-run_button = Button(l_frame, text="Get Data", padx=30, pady=5, command=check_inputs)
-run_button.place(relx=.5, rely=.7, anchor='center')
+    # label and input field diplay
+    year_label = Label(l_frame, text="Input Year", bg='#A9DFBF')
+    year_label.place(relx = 0.2, rely = 0.35, anchor='center')
+    e_year = Entry(l_frame, width=35, borderwidth=3)
+    e_year.place(relx=.5, rely=.4, anchor='center')
+    state_label = Label(l_frame, text="Input State", bg='#A9DFBF')
+    state_label.place(relx = 0.2, rely = 0.50, anchor='center')
+    e_state = Entry(l_frame, width=35, borderwidth=3)
+    e_state.place(relx=.5, rely=.55, anchor='center')
+    run_button = Button(l_frame, text="Get Data", padx=30, pady=5, command=check_inputs)
+    run_button.place(relx=.5, rely=.7, anchor='center')
+
+    menu_bar = Menu(root)
+    file_menu = Menu(menu_bar, tearoff=0)
+    file_menu.add_command(label="New", command=clear_inputs)
+    file_menu.add_command(label="Open", command=read_data)
+    file_menu.add_command(label="Save", command=write_data)
+    file_menu.add_separator()
+    file_menu.add_command(label="Exit", command=close)
+    menu_bar.add_cascade(label="File", menu=file_menu)
+    root.config(menu=menu_bar)
 
 
 def read_data():
@@ -239,17 +259,28 @@ def open_file():
     print(root.filename)
 
 
-menu_bar = Menu(root)
-file_menu = Menu(menu_bar, tearoff=0)
-file_menu.add_command(label="New", command=clear_inputs)
-file_menu.add_command(label="Open", command=read_data)
-file_menu.add_command(label="Save", command=write_data)
-file_menu.add_separator()
-file_menu.add_command(label="Exit", command=close)
-menu_bar.add_cascade(label="File", menu=file_menu)
-root.config(menu=menu_bar)
 
-read_data()
+
+#read_data()
 
 if __name__ == '__main__':
-    root.mainloop()
+
+    input_file = None
+    if sys.argv[0] != sys.argv[-1]:
+        input_file = sys.argv[-1]
+
+    if input_file:
+        with open(input_file, 'r') as f:
+            reader = csv.reader(f)
+            for row in reader:
+                if len(row) > 1:
+                    print(row)
+                    year = row[0]
+                    state = row[1]
+
+
+    #else:
+        #gui()
+
+
+    check_inputs(year, state)
